015f335e3f07e889aa66b763b538f3352093f897
BogdanCherichka <55302947+BogdanCherichka@users.noreply.github.com>
2019-09-17 00:05:42 +0300

    Update LightAcademyTest.cpp

--- LightAcademyTest.cpp
+++ LightAcademyTest.cpp
@@ -27,11 +27,6 @@ int main()
 	*(strAll + 5) = str6;
 	*(strAll + 6) = str7;
 
-	for (int i = 0; i < 7; i++)
-	{
-		cout << *(strAll + i) << endl;
-	}
-
 	cout << "\n";												
 	if (checkEvenNumberOfDuplicates(strAll, size)) cout << "Array has even number of duplicates." << endl;			//checking function checkEvenNumberOfDuplicates
 	else cout << "Array has odd number of duplicates." << endl;
@@ -49,39 +44,39 @@ bool checkEvenNumberOfDuplicates(const char* const* const arr, const int size)
 {
 	if (arr)
 	{	
-		char** arrCopy = nullptr;										//creating a copy of array with all-lower register for saving original data
+		char** arrCopy = nullptr;	//creating a copy of array with all-lower register for saving original data
 		arrCopy = new char* [size] {};
-		if (arrCopy)
+		if (arrCopy)	//if dynamic memory for array copy was reserved
 		{
 			for (int i = 0; i < size; i++)
 			{			
-				*(arrCopy + i) = nullptr;
+				*(arrCopy + i) = nullptr;	//reserving dynamic memory for every line of array copy
 
-				while (*(arrCopy + i) == nullptr)
+				while (*(arrCopy + i) == nullptr)	//if dynamic memory for every line of array copy was reserved
 				{
 					*(arrCopy + i) = new char[15]{};
 				}								
-				strcpy_s(*(arrCopy + i), (int)strlen(*(arr + i)) + 1, *(arr + i));
+				strcpy_s(*(arrCopy + i), (int)strlen(*(arr + i)) + 1, *(arr + i));	//copying content of original array to array copy
 				
 				for (int j = 0; j < (int)strlen(*(arrCopy + i)); j++)
 				{
-					*(*(arrCopy + i) + j) = tolower(*(*(arrCopy + i) + j));
+					*(*(arrCopy + i) + j) = tolower(*(*(arrCopy + i) + j));		//shifting to lower register every symbol in the line
 				}
 			}
 		}
 		
 		for (int i = 0; i < size; i++)
 		{
-			int counter{};
+			int counter{};	//creating variable that counts number of duplicates
 
 			for (int j = 0; j < size; j++)
 			{
-				if (!strncmp(*(arrCopy + i), *(arrCopy + j), strlen(*(arrCopy + i)))) counter++;
+				if (!strncmp(*(arrCopy + i), *(arrCopy + j), strlen(*(arrCopy + i)))) counter++; 	//comparing i line with every other lines
 			}
 
 			if (counter % 2 == 0)
 			{
-				for (int i = 0; i < size; i++)					// deleting created copy of array
+				for (int i = 0; i < size; i++)					// deleting created array copy before returning true
 				{
 					delete[] * (arrCopy + i);
 				}
@@ -90,13 +85,13 @@ bool checkEvenNumberOfDuplicates(const char* const* const arr, const int size)
 			}
 		}
 		
-		for (int i = 0; i < size; i++)							// deleting created copy of array
+		for (int i = 0; i < size; i++)							// deleting created array copy before returning false
 		{
 			delete[] * (arrCopy + i);
 		}
 		delete[] arrCopy;
 		return false;
 	}
-	cout << "Error! Array was not created." << endl;
+	cout << "Error! Array was not created." << endl;	//error message if dynamic memory was not reserved for array
 	return false;
 }
